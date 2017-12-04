1b8c03a783baaf2969ffc1aefb98f174473c9c02
Stephen Devine <ste@stedevine.com>
2017-12-03 19:37:48 -0800

    start to implement cases

--- day3/spiral-memory.py
+++ day3/spiral-memory.py
@@ -38,6 +38,85 @@ def getShell(inputValue):
         if inputValue <= tup[1]:
             return tup[0]
 
+# Part 2: Grid gets more interesting
+# Do we have to generate the grid?
+
+#1
+#2 1
+#3  2 (1 + (1))
+#4  4 (2 + (1 + 1))
+#5  5 (4 + 1)
+#6 10 (5 + (4 + 1))
+#7 11 (10 + 1)
+#8 23 (11 + (10 + 1))
+#9 25 (23 + (1 + 1))
+#10 26 (25 + (1))
+#11 54 (26 + (25 + 1)+2)
+#12 57
+
+def getNeighborsIndices(inputIndex):
+    shell = getShell(inputIndex)
+
+    lower = ((((shell-1)*2)-1)**2) + 1
+    upper = (((shell)*2)-1)**2
+    tr = lower + ((upper-lower)/4)
+    tl = tr + ((upper-lower)/4) + 1
+    bl = tl + ((upper-lower)/4) + 1
+    br = bl + ((upper-lower)/4) + 1
+    print "tl " + str(tl)
+
+    if (inputIndex == upper):
+        print "bottom right"
+
+    if (inputIndex == lower):
+        print "bottom right + one"
+
+
+    #if ((inputIndex > lower) and (lower < tr - 1)):
+    #    print ("right side")
+        # For shell > 2 Neighbors are previous + three values from right side of inner shell
+        # For shell == 2 Neighbours are previous + center
+
+    if (inputIndex == tr - 1):
+        #Neighbors are inner shell's top right, inner shell's topright - 1 and previous index
+      print ("top right - 1")
+
+    if (inputIndex == tr):
+        #Neighbors are inner shells top right and previous index
+        return [inputIndex-1, getInnerShellIndex(shell-1, 'tr')]
+
+    if (inputIndex == tl):
+        #Neighbors are inner shells top left and previous index
+        return [inputIndex-1, getInnerShellIndex(shell-1, 'tl')]
+
+    if (inputIndex == bl):
+        #Neighbors are inner shells bottom left and previous index
+        return [inputIndex-1, getInnerShellIndex(shell-1, 'bl')]
+
+    if (inputIndex == br):
+        #Neighbors are inner shells bottom right left and previous index
+        # and this shell's lowest index
+        return [lower, inputIndex-1, getInnerShellIndex(shell-1, 'br')]
+
+
+
+def getInnerShellIndex(shell, corner):
+    lower = ((((shell-1)*2)-1)**2) + 1
+    upper = (((shell)*2)-1)**2
+
+    tr = lower + ((upper-lower)/4)
+    tl = tr + ((upper-lower)/4) + 1
+    bl = tl + ((upper-lower)/4) + 1
+    br = bl + ((upper-lower)/4) + 1
+
+    result = {
+        'tr' : tr,
+        'tl' : tl,
+        'bl' : bl,
+        'br' : br
+        }[corner]
+    return result
+
 class Test(unittest.TestCase):
     def test(self):
         self.assertEqual(getShell(1),1)
@@ -50,4 +129,14 @@ class Test(unittest.TestCase):
         self.assertEqual(getDistanceToCenter(1024),31)
         self.assertEqual(getDistanceToCenter(1),0)
 
+print(getNeighborsIndices(13))
+print(getNeighborsIndices(17))
+print(getNeighborsIndices(21))
+print(getNeighborsIndices(25))
+
+
 print(getDistanceToCenter(368078))
+print(getInnerShellIndex(3,'tl'))
+print(getInnerShellIndex(3,'tr'))
+print(getInnerShellIndex(3,'bl'))
+print(getInnerShellIndex(3,'br'))
