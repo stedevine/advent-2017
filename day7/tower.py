32ec8b8fa05bf0eb9720d559d1366b8af0edf93c
Stephen Devine <ste@stedevine.com>
2017-12-06 23:55:27 -0800

    some progress

--- day7/tower.py
+++ day7/tower.py
@@ -1,6 +1,6 @@
 import unittest
 from sets import Set
-
+from collections import Counter
 def read_file(file_name):
     """Helper function, read file return contents as string"""
     file_handle = open(file_name, 'r')
@@ -25,6 +25,34 @@ def getBottom(inputString):
 
     return programs[0]
 
+# Break the input into key (program) and value (weight + other input)
+
+def getDictionary(inputString):
+    dic = {}
+    for line in iter(inputString.splitlines()):
+        s = line.split(" (")
+        dic[s[0]] = "(" + s[1]
+
+    #print dic
+    return dic
+
+def getProgramWeight(program, dic):
+    return int(dic[program].split("(")[1].split(")")[0])
+
+def getTowerWeight(program, dic):
+    if (dic[program].find("->") == -1):
+        return getProgramWeight(program, dic)
+
+    sum = getProgramWeight(program, dic)
+    for p in getSubTowerBases(program, dic):
+        sum += getTowerWeight(p, dic)
+
+    return sum
+
+def getSubTowerBases(program, dic):
+    return dic[program].split("-> ")[1].split(", ")
+
+"""
 def getWeights(inputString):
     weights = []
     for line in iter(inputString.splitlines()):
@@ -33,11 +61,25 @@ def getWeights(inputString):
     print  weights
     return weights
 
-def getTowerWeight(program, inputString, weights):
+def getSubtowerWeight(line, input):
+    if (line.find("->") == -1):
+        return (line.split()[0], int(line.split("(")[1].split(")")[0]))
+
+    weight = 0
+
+
+    line
+
+
+def getTowerWeight(program, inputString):
     # tower weight is the wieght of the base program
     # plus the weights of any programs it references
 
+
+
     return 0
+
+"""
 
 
 class Test(unittest.TestCase):
@@ -56,6 +98,37 @@ ugml (68) -> gyxo, ebii, jptl
 gyxo (61)
 cntj (57)"""
         self.assertEqual(getBottom(inputString),"tknk")
-        self.assertEqual(getTowerWeight("ugml", getWeights(inputString), inputString),60)
+        #self.assertEqual(getDictionary(inputString),"")
+        self.assertEqual(getTowerWeight("ugml", getDictionary(inputString)),251)
+        self.assertEqual(getTowerWeight("tknk", getDictionary(inputString)),251)
+i = read_file("input.txt")
+print(getBottom(i))
 
-print(getBottom(read_file("input.txt")))
+d = getDictionary(i)
+print(getTowerWeight(getBottom(i),d))
+
+j = """pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)"""
+dj = getDictionary(j)
+
+a={}
+for p in getSubTowerBases(getBottom(j), dj):
+    a[p] = str(getTowerWeight(p, dj))
+    print "p: " + p + " " + str(a[p])
+
+print a
+val_counter = Counter(a.itervalues())
+unbalanced = [k for k, v in a.iteritems() if val_counter[v] == 1][0]
+
+print unbalanced
